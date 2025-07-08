03fbbbf4010b6346fc646340623ad4b057b249c3
AlbertSM-dev <71648626+AlbertSM-dev@users.noreply.github.com>
2025-07-08 20:34:52 +0200

    Create Clock.java

---
+++ Clock.java
@@ -0,0 +1,60 @@
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Clock  {
+	  private volatile String currentTime;
+    private volatile boolean running = true; 
+
+    public Clock() {
+        this.currentTime = getCurrentTimeString();
+    }
+
+    public String getCurrentTime() {
+        return currentTime;
+    }
+
+    public void updateTime() {
+        this.currentTime = getCurrentTimeString();
+    }
+
+    private String getCurrentTimeString() {
+    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss dd-MM-yyyy");
+        LocalDateTime date = LocalDateTime.now();
+        return formatter.format(date);
+    }
+    
+    public Runnable updateTask() {
+    	return () -> {
+            while (running) {
+                updateTime();
+                try {
+                    Thread.sleep(100); // Update every 100 milliseconds
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    running = false;
+                    System.err.println("Update thread interrupted");
+                }
+            }
+        };
+    }
+
+    public Runnable printTask() {
+    	return () -> {
+            while (running) {
+                System.out.println(getCurrentTime());
+                try {
+                    Thread.sleep(1000); // Print each 1 second
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    running = false;
+                    System.err.println("Print thread interrupted");
+                }
+            }
+        };
+    }
+
+    public void stopClock() {
+        this.running = false;
+    }    
+}
